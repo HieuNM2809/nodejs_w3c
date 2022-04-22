19ccca60a3a31798cd47d9aedd50688f493f2871
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-04-22 08:47:33 +0700

    Bai9.2_Select ( bổ xung truyền biến vào string

--- Bai9_2_Select/index.js
+++ Bai9_2_Select/index.js
@@ -7,11 +7,20 @@ var con = mysql.createConnection({
   database: "nodejs"
 });
 
-con.connect(function(err) {
+//  ==== Query
+// con.connect(function(err) {
+//   if (err) throw err;
+//   con.query("SELECT * FROM user", function (err, result, fields) {
+//     if (err) throw err;
+//     console.log(result[0]['name']);  // Xuất tên arr thứ nhất  
+//     console.log(result);             // show all data
+//   });
+// });
+
+// ==== Truyền param
+var name = 'Teo';
+var sql = 'SELECT * FROM User WHERE name = ?';
+con.query(sql, [name], function (err, result) {
   if (err) throw err;
-  con.query("SELECT * FROM user", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[0]['name']);  // Xuất tên arr thứ nhất  
-    console.log(result);             // show all data
-  });
+  console.log(result);
 });
